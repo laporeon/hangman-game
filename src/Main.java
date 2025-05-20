71711f6702063afef609b329c8430b365f5050a8
laporeon <175569928+laporeon@users.noreply.github.com>
2025-05-20 14:55:56 -0300

    feat: instanciate project and create main menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import application.HangmanGame;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        boolean exit = false;
+
+        System.out.println("" +
+                "\n" +
+                "▗▖ ▗▖ ▗▄▖ ▗▖  ▗▖ ▗▄▄▖▗▖  ▗▖ ▗▄▖ ▗▖  ▗▖     ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖\n" +
+                "▐▌ ▐▌▐▌ ▐▌▐▛▚▖▐▌▐▌   ▐▛▚▞▜▌▐▌ ▐▌▐▛▚▖▐▌    ▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌   \n" +
+                "▐▛▀▜▌▐▛▀▜▌▐▌ ▝▜▌▐▌▝▜▌▐▌  ▐▌▐▛▀▜▌▐▌ ▝▜▌    ▐▌▝▜▌▐▛▀▜▌▐▌  ▐▌▐▛▀▀▘\n" +
+                "▐▌ ▐▌▐▌ ▐▌▐▌  ▐▌▝▚▄▞▘▐▌  ▐▌▐▌ ▐▌▐▌  ▐▌    ▝▚▄▞▘▐▌ ▐▌▐▌  ▐▌▐▙▄▄▖\n" +
+                "");
+
+        Scanner scanner = new Scanner(System.in);
+        HangmanGame hangmanGame = new HangmanGame();
+
+        while (!exit) {
+            System.out.print("Press 1 to start or 0 to exit: ");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    hangmanGame.start();
+                    break;
+                case 0:
+                    hangmanGame.exit();
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again...");
+                    System.out.flush();
+                    break;
+            }
+        }
+
+        scanner.close();
     }
 }
